ff1753052e5ab04bb68b687b19ac60a0a604bd02
Fralch <ingfralch@gmail.com>
2023-10-25 17:37:25 -0500

    5. == vs === vs typeof

---
+++ 5. == vs === vs typeof/==vs===vstypeof.js
@@ -0,0 +1,40 @@
+// Triple igual (===) compara el valor y el tipo de dato
+// ejemplo
+    1 === 1 // true
+    1 === '1' // false
+    1 === Number('1') // true
+    1 === parseInt('1') // true
+    1 === parseFloat('1') // true
+
+// Doble igual (==) compara el valor sin importar el tipo de dato
+// ejemplo
+    1 == 1 // true
+    1 == '1' // true
+    1 == Number('1') // true
+    1 == parseInt('1') // true
+    1 == parseFloat('1') // true
+
+    '   ' == 0 // true
+    '\n' == 0 // true 
+    // comparar objetos
+        // [] == [] // false
+        //{} == {} // false
+
+
+ // typeof compara el tipo de dato
+    // ejemplo
+        typeof 1 // number
+        typeof '1' // string
+        typeof true // boolean
+        typeof undefined // undefined
+        typeof null // object
+        typeof [] // object
+        typeof {} // object
+        typeof function(){} // function
+        typeof NaN // number
+        typeof Infinity // number
+
+     
+
+
+
