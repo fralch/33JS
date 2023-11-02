b6b408c2f04072749d2f39395fb31c44ef552e4e
Fralch <ingfralch@gmail.com>
2023-11-02 18:23:37 -0500

    iife

---
+++ 8.  IIFE, Modules and Namespaces/IIFE.js
@@ -0,0 +1,10 @@
+// 1. IIFE: es una función que se ejecuta inmediatamente después de ser creada.
+// Self-Executing Anonymous Function
+// Self-Invoked Anonymous Function
+
+    //  ejemplo:
+    (function() {
+        console.log('Hola');
+    })(); // es una IIFE porque se ejecuta inmediatamente después de ser creada.
+
+    // Nota: las IIFE se usan para encapsular código y evitar que se mezcle con el código global o scope global.
