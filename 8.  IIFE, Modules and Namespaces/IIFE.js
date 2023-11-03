706b0a302204dbad417bfbdfa297425f74e64dcf
Fralch <ingfralch@gmail.com>
2023-11-03 18:04:34 -0500

    modules

--- 8.  IIFE, Modules and Namespaces/IIFE.js
+++ 8.  IIFE, Modules and Namespaces/IIFE.js
@@ -8,3 +8,7 @@
     })(); // es una IIFE porque se ejecuta inmediatamente después de ser creada.
 
     // Nota: las IIFE se usan para encapsular código y evitar que se mezcle con el código global o scope global.
+    // Ejemplo Jquery:
+    (function(w, d){
+        console.log(w); // window
+    })(window, document); 
---
+++ 8.  IIFE, Modules and Namespaces/modules/main.js
@@ -0,0 +1,31 @@
+// https://www.youtube.com/watch?v=0t-Le4kdaMg
+
+// import { add, substract, multiply, divide, Arithmetic } from './math.js';
+// console.log(add(1, 2));
+// console.log(substract(1, 2));
+// console.log(multiply(1, 2));
+// console.log(divide(1, 2));
+// const aritmetica = new Arithmetic();
+// console.log(`Desde la clase ${aritmetica.doble(2)}`);
+
+
+
+//  import Arithmetic from './math.js';
+// const aritmetica = new Arithmetic();
+// console.log(`Desde la clase ${aritmetica.doble(2)}`);
+
+
+
+// import math from './math.js';
+// console.log(math.add(1, 2));
+// console.log(math.substract(1, 2));
+// console.log(math.multiply(1, 2));
+// console.log(math.divide(1, 2));
+// const aritmetica = new math.Arithmetic();
+// console.log(`Desde la clase ${aritmetica.doble(2)}`);
+
+
+ import * as math from './math.js';
+console.log(math.add(1, 2));
+console.log(math.substract(1, 2));
+console.log(math.multiply(1, 2));
