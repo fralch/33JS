7eebf6cd6b837be582267ff1d815f2882fd47abd
Fralch <ingfralch@gmail.com>
2023-11-11 10:18:46 -0500

    event loop

---
+++ 9 Message Queue and Event Loop/EventLoop.js
@@ -0,0 +1,21 @@
+// Aprendiendo Event Loop
+
+// Event Loop
+// El Event Loop se encarga de que el Call Stack se ejecute en el orden
+// que corresponde, pero también se encarga de que el Callback Queue se
+// ejecute en el orden que corresponde. Es decir, que el Event Loop se
+// encarga de que el Call Stack y el Callback Queue se ejecuten en el
+// orden que corresponde.
+
+//https://www.youtube.com/shorts/x8cn5P9Qmf4
+
+console.log("Start");
+
+setTimeout(() => {
+    console.log("Timeout");
+    }, 0);
+
+Promise.resolve()
+    .then(() => console.log("Promise"));
+
+console.log("End");
