b316f2e35d7c317503b61579204c1b0f71f0f0b5
Fralch <ingfralch@gmail.com>
2023-11-15 09:35:57 -0500

    10. setTimeoutsetInterval requestAnimationFrame

---
+++ 10. setTimeout, setInterval and requestAnimationFrame/setTimeoutSetIntervalRequestAnimation.js
@@ -0,0 +1,28 @@
+/* 
+    setTimeout: Esta función se utiliza para ejecutar una función después de un período de tiempo específico. 
+    Toma dos argumentos: la función que se ejecutará y la cantidad de tiempo (en milisegundos) que se debe 
+    esperar antes de ejecutar la función.
+*/
+setTimeout(function() {
+    // Código a ejecutar después del tiempo especificado
+  }, 1000); // Espera 1 segundo (1000 milisegundos)
+
+
+  /* 
+  setInterval: Similar a setTimeout, pero ejecuta la función repetidamente cada cierto intervalo 
+  de tiempo en lugar de ejecutarla solo una vez.
+  */
+
+  setInterval(function() {
+    // Código a ejecutar en intervalos regulares
+  }, 2000); // Ejecuta cada 2 segundos (2000 milisegundos)
+
+
+  /* 
+  requestAnimationFrame: Este método le dice al navegador que quieres realizar una animación y
+    solicita que el navegador programe el repintado de la ventana para el próximo ciclo de animación.
+    El método acepta como argumento una función a la que llamar antes de repintar.
+
+  */
+    // dame un ejemplo de requestAnimationFrame
+    
