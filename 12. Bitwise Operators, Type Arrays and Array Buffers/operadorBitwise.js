ca7b1bba937d159450a4159360b121c4b158a74a
Fralch <ingfralch@gmail.com>
2023-11-23 17:18:46 -0500

    Bitwise Operators en JavaScript

---
+++ 12. Bitwise Operators, Type Arrays and Array Buffers/operadorBitwise.js
@@ -0,0 +1,25 @@
+// Bitwise Operators en JavaScript
+
+// Operador Bitwise AND
+let resultado = 5 & 3; // Resultado: 1
+
+// Operador Bitwise OR
+resultado = 5 | 3; // Resultado: 7
+
+// Operador Bitwise XOR
+resultado = 5 ^ 3; // Resultado: 6
+
+// Operador Bitwise NOT
+resultado = ~5; // Resultado: -6
+
+// Operador Bitwise Left Shift
+resultado = 5 << 1; // Resultado: 10
+
+// Operador Bitwise Right Shift
+resultado = 5 >> 1; // Resultado: 2
+
+// Operador Bitwise Zero Fill Right Shift
+resultado = 5 >>> 1; // Resultado: 2
+
+// Operador Bitwise AND Asignación
+resultado &= 3; // Resultado: 1
